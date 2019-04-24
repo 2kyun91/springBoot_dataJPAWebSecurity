31acdcbf9f3b317d2d6c6a8b8ab2fd18e01aa539
2kyun91 <kim78boy@naver.com>
2019-04-24 15:07:28 +0900

    특정 URL에 @Secured 설정

--- src/main/java/com/example/controller/SampleController.java
+++ src/main/java/com/example/controller/SampleController.java
@@ -1,11 +1,20 @@
 package com.example.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.java.Log;
 
+/*
+ * SecurityConfig 클래스에 configure() 메소드를 이용해서 특정 경로에 접근을 제어하는 여러 설정을 했지만 단순 어노테이션만을 이용해서 접근을 제한할 수 있다.
+ * 스프링 시큐리티는 @Secured 어노테이션을 제공하는데 속성 값으로 문자열의 배열이나 문자열을 이용해서 권한을 지정한다.
+ * @Secured가 제대로 작동하기 위해서 SecurityConfig 클래스에 
+ * 	- @EnableGlobalMethodSecurity 어노테이션을 추가하고
+ *  - securedEnabled 속성 값을 true로 지정해야 한다. 
+ * */
 @Controller
 @Log
 public class SampleController {
@@ -28,5 +37,17 @@ public class SampleController {
 	@RequestMapping("/admin")
 	public void forAdmin() {
 		log.info("admin");
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping("/adminSecret")
+	public void forAdminSecret() {
+		log.info("admin secret");
+	}
+	
+	@Secured("ROLE_MANAGER")
+	@RequestMapping("/managerSecret")
+	public void forManagerSecret() {
+		log.info("manager secret");
 	}
 }
--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,6 +35,7 @@ import lombok.extern.java.Log;
  * */
 @Log
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
